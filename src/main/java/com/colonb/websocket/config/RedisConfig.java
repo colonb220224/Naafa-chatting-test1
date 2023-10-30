494f1900ef3bd9c2ac619a634dd1260a1019fa03
workspace <jinsa004@colon-b.com>
2023-10-30 09:41:22 +0900

    WebSocket 외부서버 자르파일

--- src/main/java/com/colonb/websocket/config/RedisConfig.java
+++ src/main/java/com/colonb/websocket/config/RedisConfig.java
@@ -1,28 +1,28 @@
-package com.colonb.websocket.config;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
-import redis.embedded.RedisServer;
-
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
-import java.io.IOException;
-
-@Configuration
-public class RedisConfig {
-    private RedisServer redisServer;
-
-    public RedisConfig(@Value("${spring.redis.port}") int port) throws IOException {
-        this.redisServer = RedisServer.builder().port(port).setting("maxmemory 128M").build();
-    }
-
-    @PostConstruct
-    public void startRedis() {
-        this.redisServer.start();
-    }
-
-    @PreDestroy
-    public void stopRedis() {
-        this.redisServer.stop();
-    }
-}
+//package com.colonb.websocket.config;
+//
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Configuration;
+//import redis.embedded.RedisServer;
+//
+//import javax.annotation.PostConstruct;
+//import javax.annotation.PreDestroy;
+//import java.io.IOException;
+//
+//@Configuration
+//public class RedisConfig {
+//    private RedisServer redisServer;
+//
+//    public RedisConfig(@Value("${spring.redis.port}") int port) throws IOException {
+//        this.redisServer = RedisServer.builder().port(port).setting("maxmemory 128M").build();
+//    }
+//
+//    @PostConstruct
+//    public void startRedis() {
+//        this.redisServer.start();
+//    }
+//
+//    @PreDestroy
+//    public void stopRedis() {
+//        this.redisServer.stop();
+//    }
+//}
