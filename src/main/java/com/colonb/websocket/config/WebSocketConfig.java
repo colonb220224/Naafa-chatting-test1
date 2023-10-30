20cea1e0e49e358637312889cff187aa5757f6f5
workspace <jinsa004@colon-b.com>
2023-10-30 14:57:16 +0900

    WebSocket ws config 수정

--- src/main/java/com/colonb/websocket/config/WebSocketConfig.java
+++ src/main/java/com/colonb/websocket/config/WebSocketConfig.java
@@ -13,7 +13,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         // stomp 접속 주소 url => /ws-stomp
-        registry.addEndpoint("/ws-stomp") // 연결될 엔드포인트
+        registry.addEndpoint("/ws-stomp").setAllowedOriginPatterns("*") // 연결될 엔드포인트
                 .withSockJS(); // SocketJS 를 연결한다는 설정
     }
 
