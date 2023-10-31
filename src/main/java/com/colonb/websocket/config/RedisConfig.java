998bc40791af44a66c6399ac83fdf3f8248358fe
workspace <jinsa004@colon-b.com>
2023-11-01 06:20:24 +0900

    WebSocket redis config추가

--- src/main/java/com/colonb/websocket/config/RedisConfig.java
+++ src/main/java/com/colonb/websocket/config/RedisConfig.java
@@ -1,23 +1,41 @@
 package com.colonb.websocket.config;
 
+import com.colonb.websocket.dto.ChatDTO;
+import com.colonb.websocket.pubsub.RedisSubscriber;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
 
-    /**
-     * redis pub/sub 메시지를 처리하는 listener 설정
-     */
+    // ChannelTopic 등록
     @Bean
-    public RedisMessageListenerContainer redisMessageListener(RedisConnectionFactory connectionFactory) {
+    public ChannelTopic channelTopic() {
+        return new ChannelTopic("chatroom");
+    }
+
+    // MessageListenerAdapter 등록
+    @Bean
+    public MessageListenerAdapter listenerAdapter(RedisSubscriber redisSubscriber) {
+        return new MessageListenerAdapter(redisSubscriber, "sendMessage");
+    }
+
+    // 등록한 ChannelTopic, MessageListenerAdapter로 RedisMessageListenerContainer 등록
+    // redis pub/sub 메시지를 처리하는 listener 설정
+    @Bean
+    public RedisMessageListenerContainer redisMessageListener(RedisConnectionFactory connectionFactory,
+                                                              MessageListenerAdapter listenerAdapter,
+                                                              ChannelTopic channelTopic) {
         RedisMessageListenerContainer container = new RedisMessageListenerContainer();
         container.setConnectionFactory(connectionFactory);
+        container.addMessageListener(listenerAdapter, channelTopic);
         return container;
     }
 
@@ -25,11 +43,12 @@ public class RedisConfig {
      * 어플리케이션에서 사용할 redisTemplate 설정
      */
     @Bean
-    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
-        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(connectionFactory);
-        redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(String.class));
+    public RedisTemplate<String, ChatDTO> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, ChatDTO> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(connectionFactory); // connection
+        redisTemplate.setKeySerializer(new StringRedisSerializer()); // key
+        redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(Object.class)); // value
         return redisTemplate;
+    }
     }
 }
